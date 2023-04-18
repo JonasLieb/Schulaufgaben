b0e50cdb68bdd36a19935efe31789b471e80d0b4
jonas <jonas.lieben@gmx.de>
2023-04-18 22:39:14 +0200

    LS07 angefangen

---
+++ src/main/java/ls07/aufgaben/LS07_Dateispeicherung.java
@@ -0,0 +1,12 @@
+package ls07.aufgaben;
+
+public class LS07_Dateispeicherung {
+    /*
+     * Factories
+     * -In der Objektorientierten Programmierung wächst die Größe eines Objektes meist sehr schnell.
+     *  Das Erzeugen des Objektes ist fast schon aufwändig oder lästig, weil tausend Parameter
+     *  benötigt werden oder der Aufruf verschiedener Methoden nötig ist.
+     * - Um dieses Problem gibt es Factories - Meist Statische Klassen oder Methoden, die einfach nur Das gewünschte Pbjekt erzeugen.
+     * - Im schönsten Fall kann diese "getObject" Methode dann das Gewünschte Objekt mithilfe von "reflection" erzeugen.
+     * */
+}
